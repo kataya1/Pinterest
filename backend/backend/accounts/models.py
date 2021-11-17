b5c7560fbc95367f62ce9879db6eb47adcf836a7
kataya1 <mohamedkataya1@gmail.com>
2021-11-17 22:24:31 +0200

    good old copy past 😅

--- backend/backend/accounts/models.py
+++ backend/backend/accounts/models.py
@@ -1,14 +1,25 @@
 from django.db import models
-
-# Create your models here.
-
-#model user
+from django.contrib.auth.models import AbstractUser
+from pins.models import Topic
 
 
+class User(AbstractUser):
+    
+    date_of_birth = models.DateField(null=True)
+    business = models.BooleanField(default=False)
+    country = models.CharField(max_length=50, null=True)
+    gender = models.CharField(
+        choices=(('male', 'm'), ('female', 'f')), max_length=50, default="male")
+    avatar = models.ImageField(
+        upload_to="profilePic/%y/%m/%d", null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    # add interset as many to many field
+    # circular import
+    interest = models.ManyToManyField(Topic)
 
-#model pins
+    # first issue
+    following = models.ManyToManyField(
+        'self', related_name='followers', blank=True)
 
-
-
-#model history
-
+    def __str__(self):
+        return self.username
