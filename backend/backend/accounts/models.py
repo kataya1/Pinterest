2dede831b0e10dd487db84246324e7672d09c4bf
kataya1 <mohamedkataya1@gmail.com>
2021-11-17 19:38:08 +0200

    sorry for the troubles😔

--- backend/backend/accounts/models.py
+++ backend/backend/accounts/models.py
@@ -4,22 +4,22 @@ from pins.models import Topic
 
 
 class User(AbstractUser):
-    dateOfBirth = models.DateField(null=True)
-    # delete active
-    # active = models.BooleanField(default=True)
+    
+    date_of_birth = models.DateField(null=True)
     business = models.BooleanField(default=False)
     country = models.CharField(max_length=50, null=True)
     gender = models.CharField(
         choices=(('male', 'm'), ('female', 'f')), max_length=50, default="male")
-    # d will be lowercase
     avatar = models.ImageField(
         upload_to="profilePic/%y/%m/%d", null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     # add interset as many to many field
+    # circular import
     interest = models.ManyToManyField(Topic)
+
     # first issue
-    followers = models.ManyToManyField(
-        'self', related_name='follower', blank=True)
+    following = models.ManyToManyField(
+        'self', related_name='followers', blank=True)
 
     def __str__(self):
         return self.username
