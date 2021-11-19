3c93e7e94b54368b4501c8a088bd5c63a828cc55
kataya1 <mohamedkataya1@gmail.com>
2021-11-19 12:13:50 +0200

    npm start fully working

--- frontend/pintrest/src/App.js
+++ frontend/pintrest/src/App.js
@@ -1,9 +1,23 @@
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Page/>
+      <div className="words">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat harum veritatis impedit architecto. Dicta iste consequatur ipsum veritatis illo aliquam debitis quibusdam hic sequi quidem, vitae deleniti, delectus dignissimos quae.
+        Veniam sed debitis culpa error, quo praesentium repudiandae nemo temporibus ipsum perspiciatis doloribus esse nam fugiat labore natus non ullam similique rerum numquam saepe dolore fuga reiciendis quasi. Consequatur, dolor!
+        Earum magnam sint minima mollitia numquam! Accusantium inventore laborum voluptatem officiis odio vel in autem modi earum mollitia dolores repellat, aliquid eum facilis eos! Atque sequi quisquam odio commodi veniam?
+        Sit molestiae perferendis consequatur numquam velit aliquid laboriosam dignissimos provident possimus quia totam suscipit soluta voluptate, ipsa fuga hic necessitatibus? Rerum quas architecto repudiandae, laboriosam veniam quidem id officiis voluptatibus.
+        Similique, perspiciatis repudiandae itaque neque ipsum natus consequatur architecto excepturi rem incidunt ex alias non aspernatur iure, aut porro? Perferendis nesciunt veritatis debitis quasi nam necessitatibus doloremque eum, delectus expedita.
+        Debitis recusandae id autem dolores placeat doloremque, expedita soluta quos voluptas fugiat quam provident, repudiandae itaque, illum consequatur reprehenderit totam officiis doloribus rerum quis ullam quidem in animi? Dolor, reiciendis.
+        Sit, incidunt est velit optio doloremque ab aspernatur. Fugiat, maiores, laudantium reiciendis possimus vero qui odio saepe officia incidunt natus necessitatibus iure ipsa dolore voluptatem commodi, temporibus quas voluptatibus. Provident!
+        Unde ea recusandae placeat iure at inventore, sint dolore eos ex dignissimos, repellat aspernatur voluptatum alias id nisi. Dignissimos atque quaerat perferendis nam maiores aliquam nesciunt autem facere temporibus nulla.
+        Soluta illum aperiam nisi neque debitis, iure reiciendis, ipsam quod impedit quam necessitatibus. Rem recusandae sit soluta numquam natus veritatis odio blanditiis, ratione cupiditate exercitationem veniam pariatur dolore provident delectus?
+        Magnam maiores nisi omnis, facere enim animi, dolor ducimus, odit distinctio expedita doloribus suscipit possimus? Tempora unde officiis repellendus reiciendis nihil, inventore deserunt nulla dignissimos tempore, expedita, explicabo quisquam illo.
+        Suscipit, dolorum pariatur reiciendis veniam, at fuga officiis quos rem consectetur architecto dolor expedita quae, temporibus laboriosam fugit minima excepturi rerum ullam. Illo facere totam cupiditate, suscipit nostrum unde rerum.
+        Corrupti, neque tempora odio eos quis molestias quas nulla vitae dolorum tenetur autem est accusamus veniam, suscipit esse alias temporibus eaque repellat saepe aliquam minima accusantium ipsum. Corrupti, eius consequatur.
+      </div>
     </>
   );
 }
