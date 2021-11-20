422f2c20c12d7ead55c52767bc803d4358e5d9d7
Hassan Tarek <hassantarekha@gmail.com>
2021-11-20 11:47:37 +0200

    upload navbar

--- frontend/pintrest/src/components/Navbar/Navbar.js
+++ frontend/pintrest/src/components/Navbar/Navbar.js
@@ -1,11 +1,31 @@
-import styles from './Navbar.module.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPinterest} from '@fortawesome/free-brands-svg-icons'
+import { faBell,faCommentDots,faSearch,faChevronDown,faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Avatar from '../Avatar/Avatar'
+import ButtonFlex from '../Button/ButtonFlex'
+
+import '../Navbar/Navbar.css'
 import Searchbox  from '../Searchbox/Searchbox'; 
+
+library.add(faPinterest,faBell,faCommentDots,faSearch,faChevronDown,faUser)
+
+
 export default function Navbar() {
     
     return (
-    <nav className={styles.navigation}>
+    <nav>
+        <div className='left'>
+            <ButtonFlex buttonStyle="btn--logo"><FontAwesomeIcon icon={['fab', 'pinterest']} size="2x"/></ButtonFlex>
+            <ButtonFlex buttonStyle='btn--text'>Home</ButtonFlex>
+        </div>
         <Searchbox></Searchbox>
-        <button>hello</button>
+        <div className='right'>
+            <ButtonFlex buttonStyle="btn--icon"><FontAwesomeIcon icon="bell" size="2x"/></ButtonFlex>
+            <ButtonFlex buttonStyle="btn--icon"><FontAwesomeIcon icon="comment-dots" size="2x"/></ButtonFlex>
+            <Avatar src="https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1488&q=80"/>
+            <ButtonFlex buttonStyle="btn--down"><FontAwesomeIcon icon="chevron-down"/></ButtonFlex>
+        </div>
     </nav>
     )
 }
