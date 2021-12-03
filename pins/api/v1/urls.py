931f0df485cd3e5b45aa6c4e59c13ef03b3ee5cc
kataya1 <mohamedkataya1@gmail.com>
2021-12-03 21:45:12 +0200

    adding backend/api status endpoint

--- pins/api/v1/urls.py
+++ pins/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import  home, create_pin, save_pin,history, delete_history
+from .views import  home, create_pin, save_pin,history, delete_history, api_status
 app_name="pins"
 urlpatterns = [
-path('home/', home, name='home'),
+    path('home/', home, name='home'),
     path('pin/create/', create_pin, name='create'),
     path('pin/save/', save_pin, name='save'),
     path('profile/history/', history, name='history'),
     path('profile/history/<int:id>', delete_history, name='delete_history'),
+     path('api_status/', api_status, name="status"),
 ]
