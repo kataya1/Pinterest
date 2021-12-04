24d5ebc8d5ab45721f7a89fcc91ef101c252c2e1
HassanTAli <hassantarekha@gmail.com>
2021-12-04 17:56:16 +0200

    add coming soon banner to unused btns

--- frontend/pintrest/src/components/Settings/SideBar/Pages/AccountSettings.js
+++ frontend/pintrest/src/components/Settings/SideBar/Pages/AccountSettings.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Authcontext } from "../../../Authentication/Authcontext";
+import { injectTooltip } from "../../../Tooltip/Tooltip";
 import axios from "axios";
 
 import styles from './pages.module.css'
@@ -104,7 +105,10 @@ const AccountSettings = () => {
                         <div></div>
                         <div>
                             {/* <button className={`${styles.buttonSettings} ${styles2.mr1} ${styles2.mt1}`}>Reset</button> */}
-                            <button className={styles2.buttonSettings} type="submit">Save</button>
+                            <button 
+                            className={styles2.buttonSettings} 
+                            onClickCapture={injectTooltip}
+                            type="submit">Save</button>
                         </div>
                         <div className={styles.questionBtn}><svg className={styles.svgLogo} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14Z" />
