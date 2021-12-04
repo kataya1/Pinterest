ba0dfc29ad6441da495236b67539740c4536b943
kataya1 <mohamedkataya1@gmail.com>
2021-12-04 17:40:00 +0200

    fixing tooltip positions when screen changes size

--- frontend/pintrest/src/components/Tooltip/Tooltip.jsx
+++ frontend/pintrest/src/components/Tooltip/Tooltip.jsx
@@ -3,6 +3,10 @@ import styles from './Tooltip.module.css'
 import ReactDOM from 'react-dom';
 
 export const injectTooltip = (e) => {
+
+    console.log(e.pageX)
+    console.log(e.pageY)
+    console.log(e)
     function Tooltip(props) {
         const [visible, setvisible] = useState(true)
         const tooltip  = useRef(null)
@@ -13,19 +17,25 @@ export const injectTooltip = (e) => {
     
         useEffect(() => {
             if(visible){
-                
                 tooltip.current.className = [styles.tooltip, styles.visible].join(" ")
             }else{
                 tooltip.current.className = [styles.tooltip, styles.invisible].join(" ")
             }
-        })
+
+            return ()=>{
+                console.log("tooltip aborted")
+            }
+        },[visible])
+
+
         return (
             <div ref={tooltip} className={styles.tooltip} style={{top: `${props.posY - 60}px`, left: `${+props.posX + 10}px`}}>
                 coming soon...rr
             </div>
         )
     }
-    ReactDOM.render(<Tooltip posX={e.pageX} posY={e.pageY} />,document.querySelector("#tooltipspan"))
+
+    ReactDOM.render(<Tooltip posX={e.clientX} posY={e.clientY} />,document.querySelector("#tooltipspan"))
 }
 
 
